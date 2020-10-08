d8886113281e23f35500ce2b853a42cd768e7743
Karol Trzcinski <k.trzcinski95@gmail.com>
2020-10-08 17:51:51 +0200

    Basic structures definition
    Signed-off-by: Karol Trzcinski <k.trzcinski95@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use serde::{Serialize, Deserialize};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -40,3 +41,52 @@ macro_rules! console_log {
 pub fn greet() {
     alert("Hello, rtls!");
 }
+
+//
+// Define WASM structures
+//
+
+const MEASURE_DEPTH : usize = 5;
+const POSITION_TRACE_DEPTH : usize = 3;
+
+#[wasm_bindgen]
+#[derive(Serialize, Deserialize)]
+#[derive(Copy, Clone)]
+pub struct Trace {
+    cord: [f32; 3],
+    timestamp: u32,
+}
+
+#[wasm_bindgen]
+#[derive(Serialize, Deserialize)]
+pub struct Device {
+    trace: VecDeque<Trace>,
+    id: u32,
+    timestamp: u32, // last activity timestamp
+}
+
+#[wasm_bindgen]
+pub struct Measure {
+    id: [u32; 2],
+    timestamp: u32, // time can't be passed as float, because of rounding effect
+    distance: f32,
+}
+
+#[wasm_bindgen]
+pub struct MeasureList {
+    dev: [u32; 2],
+    measures_ts: [u32; MEASURE_DEPTH],
+    measures_val: [f32; MEASURE_DEPTH],
+}
+
+#[wasm_bindgen]
+pub struct Zone {
+    id: i32,
+    measures: Vec<MeasureList>,
+    devices: Vec<Device>,
+}
+
+//
+// Define private structures
+//
+
