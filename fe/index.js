97eb12e488e83f7927055e9f96967e9c1011fba5
Karol Trzcinski <k.trzcinski95@gmail.com>
2020-10-08 17:52:05 +0200

    js: Add possibility to move and zoom map
    Signed-off-by: Karol Trzcinski <k.trzcinski95@gmail.com>

--- fe/index.js
+++ fe/index.js
@@ -15,21 +15,77 @@ zone.add_device(2, 300, 270, 0);
 zone.add_measure(1, 2, 200, 123);
 zone.add_measure(1, 2, 220, 127);
 
+var map = {zoom:0.5, rotation:0.0, offset:[0,dev_canvas.height], moving:false};
+
+dev_canvas.onwheel = (event) => {
+        map.zoom += event.deltaY * -0.0001;
+        map.zoom = Math.min(Math.max(0.05, map.zoom), 10);
+}
+dev_canvas.onmousedown = () => {
+        map.moving = true;
+}
+dev_canvas.onmouseup = () => {
+        map.moving = false;
+}
+dev_canvas.onmousemove = (event) => {
+        if (map.moving) {
+                map.offset[0] += event.movementX;
+                map.offset[1] += event.movementY;
+        }
+}
+
+function getLegendResolition() {
+        let resolution_in_pixels = 10;
+        let range = dev_canvas.width / map.zoom / resolution_in_pixels;
+        let exponent = Math.floor(Math.log10(range));
+        let fraction = range / Math.pow(10, exponent);
+        var niceFraction = 0;
+
+        if (fraction < 1.5)
+                niceFraction = 1;
+        else if (fraction < 3)
+                niceFraction = 2;
+        else if (fraction < 7)
+                niceFraction = 5;
+        else
+                niceFraction = 10;
+        let real_resolution = niceFraction * Math.pow(10, exponent);
+        let pixels_resolution = real_resolution * map.zoom;
+        return {real: real_resolution, pixels: pixels_resolution};
+}
+
+function getStartOffsets(offset_pix, resolution) {
+        let off_real = offset_pix / map.zoom;
+        let off_real_n = Math.floor((-off_real) / resolution["real"]) * resolution["real"];
+        let off_pix = offset_pix % resolution["pixels"];
+        if (off_pix <= 0) {
+                off_pix += resolution["pixels"];
+        }
+        return {real: off_real_n, pixels: off_pix};
+}
+
 const drawGrid = () => {
         ctx.beginPath();
         ctx.strokeStyle = "#CCCCCC";
         ctx.font = "12px Arial";
         ctx.textAlign = "left";
+        let resolution = getLegendResolition();
+        let offset = [getStartOffsets(map.offset[0], resolution),
+                      getStartOffsets(map.offset[1], resolution)];
         // draw grid
-        for (let col = 0; col < dev_canvas.width; col += grid_resolution) {
-                ctx.moveTo(col, 0);
-                ctx.lineTo(col, dev_canvas.height);
-                ctx.fillText(col, col, dev_canvas.height - 6);
+        for (let col = 0; col < dev_canvas.width / resolution["pixels"]; col += 1) {
+                let col_px = col * resolution["pixels"] + offset[0]["pixels"]
+                let label = Math.round(pixelsToPos([col_px, 0])[0] / resolution["real"]) * resolution["real"];
+                ctx.moveTo(col_px, 0);
+                ctx.lineTo(col_px, dev_canvas.height);
+                ctx.fillText(label, col_px, dev_canvas.height - 6);
         }
-        for (let row = 0; row < dev_canvas.height; row += grid_resolution) {
-                ctx.moveTo(0, row);
-                ctx.lineTo(dev_canvas.width, row);
-                ctx.fillText(row, 0, dev_canvas.height - row + 6);
+        for (let row = 0; row < dev_canvas.height / resolution["pixels"]; row += 1) {
+                let row_px = row * resolution["pixels"] + offset[1]["pixels"]
+                let label = Math.round(pixelsToPos([0, row_px])[1] / resolution["real"]) * resolution["real"]
+                ctx.moveTo(0, row_px);
+                ctx.lineTo(dev_canvas.width, row_px);
+                ctx.fillText(label, 0, row_px);
         }
         // Draw border
         ctx.moveTo(0, 0);
@@ -56,9 +112,26 @@ function get_img(id)
         return get_img.list[id % get_img.list.length];
 }
 
-function posToPixels(pos, border) {
-        var np = [pos[0], dev_canvas.height-pos[1]];
+function posToPixels(pos) {
+        var np = [pos[0], -pos[1]]; // invert Y
+         // zoom and translation
+        np = [np[0] * map.zoom + map.offset[0],
+              np[1] * map.zoom + map.offset[1]];
+        return np;
+}
+
+function pixelsToPos(px) {
+        var pos = [px[0], px[1]];
+         // translation and zoom
+        pos = [(pos[0] - map.offset[0]) / map.zoom,
+               (pos[1] - map.offset[1]) / map.zoom];
+        pos = [pos[0], -pos[1]]; // invert Y
+        return pos;
+}
+
+function stickToBorders(pos, border) {
         var real_position = true;
+        var np = [pos[0], pos[1]];
         if (border > 0) {
                 np[0] = Math.max(border/2, Math.min(dev_canvas.width-border/2, np[0]));
                 np[1] = Math.max(border, Math.min(dev_canvas.height-border, pos[1]));
@@ -77,7 +150,8 @@ const drawDevices = () => {
         ctx.textAlign = "center";
 
         devs.forEach((elem) => {
-                let [pos, real_position] = posToPixels(elem.pos.cord, SIZE);
+                let pixels_pos = posToPixels(elem.pos.cord);
+                let [pos, real_position] = stickToBorders(pixels_pos, SIZE);
                 if (real_position)
                         ctx.globalAlpha = 1.0;
                 else
